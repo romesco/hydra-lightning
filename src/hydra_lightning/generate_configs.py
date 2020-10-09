e52a534ef06ebb6aa6d605f8795af6a8684cbaca
Rosario Scalise <rosario@cs.washington.edu>
2020-10-08 23:14:16 -0700

    basic structure

---
+++ src/hydra_lightning/generate_configs.py
@@ -0,0 +1,13 @@
+import hydra
+from configen.config import Config
+from configen.configen import generate_module, save
+
+@hydra.main(config_name="conf/configen")
+def main(cfg: Config):
+    print(cfg)
+    for module in cfg.configen.modules:
+        code = generate_module(cfg=cfg.configen, module=module)
+        save(cfg=cfg.configen, module=module.name, code=code)
+
+if __name__ == "__main__":
+    main()
