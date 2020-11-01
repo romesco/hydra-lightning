64a9390720c42fdb64f69bb2dfe953c0447f95e3
Rosario Scalise <rosario@cs.washington.edu>
2020-11-01 15:48:27 -0800

    update test

--- test_imports.py
+++ test_imports.py
@@ -4,11 +4,15 @@ from dataclasses import fields
 # legacy/hydra_configs/pytorch_lightning_v091/trainer
 import hydra_configs.pytorch_lightning
 print(fields(hydra_configs.pytorch_lightning.TrainerConf)[0])
+# prints old trainer config
+
 
 # this however, does not
 from hydra_configs.pytorch_lightning.trainer import TrainerConf
 print(fields(TrainerConf)[0])
+# prints new trainer config (we want old)
 
 # this works as expected:
 from hydra_configs.pytorch_lightning_v091.trainer import TrainerConf
 print(fields(TrainerConf)[0])
+# prints old trainer config
