27843d5852127a8c05cc7f2a6fa98c5139a1fee7
romesc <rosario@cs.washington.edu>
2021-01-21 11:54:55 -0800

    Add TrainerConf test

--- hydra-configs-pytorch-lightning/tests/test_instantiate_trainer.py
+++ hydra-configs-pytorch-lightning/tests/test_instantiate_trainer.py
@@ -1,41 +1,36 @@
-# from typing import Any
-# import pytest
-# from hydra.utils import get_class
-# from hydra.utils import instantiate
-# from omegaconf import OmegaConf
-# @pytest.mark.parametrize(
-#    "modulepath, classname, cfg, passthrough_args, passthrough_kwargs, expected",
-#    [
-#        pytest.param(
-#            "nn.modules.loss",
-#            "BCELoss",
-#            {},
-#            [],
-#            {"weight": Tensor([1])},
-#            loss.BCELoss(),
-#            id="BCELossConf",
-#        ),
-#    ],
-# )
-# def test_instantiate_classes(
-#    modulepath: str,
-#    classname: str,
-#    cfg: Any,
-#    passthrough_args: Any,
-#    passthrough_kwargs: Any,
-#    expected: Any,
-# ) -> None:
-#    full_class = f"hydra_configs.torch.{modulepath}.{classname}Conf"
-#    schema = OmegaConf.structured(get_class(full_class))
-#    cfg = OmegaConf.merge(schema, cfg)
-#    obj = instantiate(cfg, *passthrough_args, **passthrough_kwargs)
-#
-#    assert isinstance(obj, type(expected))
+from typing import Any
+
+import pytest
+import pytorch_lightning as pl
+from hydra.utils import get_class
+from hydra.utils import instantiate
+from omegaconf import OmegaConf
 
 
-def f():
-    return 4
-
-
-def test_function():
-    assert f() == 4
+@pytest.mark.parametrize(
+    "modulepath, classname, cfg, passthrough_args, passthrough_kwargs, expected",
+    [
+        pytest.param(
+            "trainer",
+            "Trainer",
+            {},
+            [],
+            {},
+            pl.trainer.Trainer(),
+            id="TrainerConf",
+        ),
+    ],
+)
+def test_instantiate_classes(
+    modulepath: str,
+    classname: str,
+    cfg: Any,
+    passthrough_args: Any,
+    passthrough_kwargs: Any,
+    expected: Any,
+) -> None:
+    full_class = f"hydra_configs.pytorch_lightning.{modulepath}.{classname}Conf"
+    schema = OmegaConf.structured(get_class(full_class))
+    cfg = OmegaConf.merge(schema, cfg)
+    obj = instantiate(cfg, *passthrough_args, **passthrough_kwargs)
+    assert isinstance(obj, type(expected))
